a1548598ef59d28c643b9a0399facfc5b53e6673
Elisha <alvinjohnson@Elishas-MacBook-Pro.local>
2018-02-26 12:50:45 +0100

    Initial commit

---
+++ Animal.java
@@ -0,0 +1,124 @@
+import java.util.*;
+
+public class Animal {
+  
+  // lav pseudorandom objekt
+  Random r = new Random();
+  Scanner console = new Scanner(System.in);
+   
+  // attributter
+  private String name;
+  private String location;
+  
+  private int x1 = r.nextInt(9)+1;
+  private int y1 = r.nextInt(9)+1;
+  private int x2 = r.nextInt(9)+1;
+  private int y2 = r.nextInt(9)+1;  
+  
+  // constructor med navneparameter
+  public Animal(String name) {
+   this.name = name;
+  }
+  
+  // set metoder
+  public void setName(String animal) {
+   name = animal;   
+  }
+  
+  public void moveX(int x) {
+   int moveX = r.nextInt(9)+1;
+  }
+  
+  public void moveY(int y) {
+   int moveY = r.nextInt(9)+1;
+  }
+  
+  public void runGame() {
+   int x1 = 1;
+   int y1 = 1;
+   int x2 = 9;
+   int y2 = 9;
+   int count = 0;
+   while (! ((x1 == x2) && (y1 == y2))) {
+      count++;
+      if (r.nextInt(2)+1 == 1) {
+      x1 = x1+1;
+      }
+      else {
+      x1 = x1-1; 
+      }   
+      
+      if (r.nextInt(2)+1 == 1) {
+      x2 = x2+1;
+      }
+      else {
+      x2 = x2-1;
+      }            
+      
+      if (r.nextInt(2)+1 == 2) {
+      y1 = y1+1;
+      }
+      else {
+      y1 = y1-1;
+      }
+      
+      if (r.nextInt(2)+1 == 2) {
+      y2 = y2+1;
+      }
+      else {
+      y2 = y2-1;
+      }
+      
+      // begrænsning af første kvadrant
+      if (x1 == 10) {
+      x1 = x1 - 2;
+      }
+      if(x1 == 0) {
+      x1 = x1 + 2;
+      }
+      if (x2 == 10) {
+      x2 = x2 - 2;
+      }
+      if (x2 == 0) {
+      x2 = x2 + 2;
+      }
+      if (y1 == 10) {
+      y1 = y1 - 2;
+      }  
+      if (y1 == 0) {
+      y1 = y1 + 2;
+      }  
+      if (y2 == 10) {
+      y2 = y2 - 2;
+      }
+      if (y2 == 0) {
+      y2 = y2 + 2;
+      }   
+      
+      // står et ryk fra hinanden
+      double a = Math.pow(x2-x1,2)+Math.pow(y2-y1,2);
+      double b = Math.sqrt(a);
+      double c = Math.sqrt(2);
+      
+      if (b <= c) {
+         System.out.println("Rabbit: Arrgh! Mr. Snake, please do not eat me!");
+      }
+      
+      // står på samme felt
+      if ((x1 == x2) && (y1 == y2)) {
+         System.out.println("I am the Rabbit and I stand on point (" + x1 + ", " + y1 + ")");
+         System.out.println("I am the Snake and I stand on point (" + x2 + ", " + y2 + ")");
+         System.out.println("Rabbit: Arrgh! Mr. Snake, please do not eat me!");
+         System.out.println("Snake: I am hungry, so I'm going to eat you!");  
+      }
+      else {
+         System.out.println("I am the Rabbit and I stand on point (" + x1 + ", " + y1 + ")");     
+         System.out.println("I am the Snake and I stand on point (" + x2 + ", " + y2 + ")");
+      }     
+   }
+   System.out.println();
+   System.out.println("***********GAME OVER*************");
+   System.out.println("The Rabbit was eaten by the Snake.");
+   System.out.println("You survived for " + count + " rounds.");
+  }
+}
---
+++ Game.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Game {
+   public static void main(String [] args) {
+   
+   // lav dyr
+   Animal snake = new Animal("Snake");
+   Animal rabbit = new Animal("Rabbit");
+   
+  // console input
+   Scanner console = new Scanner(System.in);
+   
+   System.out.println("Welcome to the game \"Bunny Hop\".");
+   System.out.println("Let us see how many turns you are able to stay away and not be eaten by the snake.");  
+   System.out.println("Type \'start\' to start the game.");
+   while (!console.hasNext("start")) {
+      System.out.println("You have made a typo. Try again.");
+      console.next(); 
+   }
+   
+   snake.runGame();
+   }
+}
